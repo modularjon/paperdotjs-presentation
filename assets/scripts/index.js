e11183e64eed832c01d9eb0e46b7db1a17d28052
modularjon <modularjon@gmail.com>
2016-07-19 12:20:17 -0400

    Invoke event handler for paper on doc ready

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,5 +3,12 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+const paperEvents = require('./paper.js');
+
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// On document ready
+$(() => {
+  paperEvents.addHandlers();
+});
