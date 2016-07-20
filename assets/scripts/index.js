f65d0cf11bbc5958757d4312466999d410328eeb
modularjon <modularjon@gmail.com>
2016-07-20 15:35:13 -0400

    Add paths and styling for 'smiley' face

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,13 +3,13 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
-const paperSetupEvents = require('./paper/paper-setup');
+// const paperSetupEvents = require('./paper/paper-setup');
 // const paperRotateEvents = require('./paper/paper-rotate');
 // const paperToolEvents = require('./paper/paper-tools');
 // const paperMultiToolEvents = require('./paper/paper-multitools');
 // const paperCircleEvents = require('./paper/paper-circle');
 // const paperColorEvents = require('./paper/paper-color');
-// const paperSmileyFace = require('./paper/paper-smiley-face');
+const paperSmileyFace = require('./paper/paper-smiley-face');
 
 // use require without a reference to ensure a file is bundled
 require('./example');
@@ -17,11 +17,11 @@ require('./example');
 // On document ready
 $(() => {
   paper.install(window);
-  paperSetupEvents.addHandlers();
+  // paperSetupEvents.addHandlers();
   // paperRotateEvents.addHandlers();
   // paperToolEvents.addHandlers();
   // paperMultiToolEvents.addHandlers();
   // paperCircleEvents.addHandlers();
   // paperColorEvents.addHandlers();
-  // paperSmileyFace.addHandlers();
+  paperSmileyFace.addHandlers();
 });
--- assets/scripts/paper/paper-smiley-face.js
+++ assets/scripts/paper/paper-smiley-face.js
@@ -7,6 +7,44 @@ const paperSmileyFace = () => {
   // Create empty project and view for the canvas:
   paper.setup('myCanvas');
 
+  let smiley = new Path.Circle([200, 200], 100);
+
+  let smileyEyeLeft = new Path.Circle([165, 175], 20);
+
+  let smileyStyle = {
+    strokeColor: 'black',
+    fillColor: 'yellow',
+    strokeWidth: 15
+  };
+
+  let smileyEyeStyle = {
+    strokeColor: 'black',
+    fillColor: 'black'
+  };
+
+  let smileyMouthStyle = {
+    strokeColor: 'black',
+    strokeWidth: 20
+  };
+
+  smiley.style = smileyStyle;
+
+  smileyEyeLeft.style = smileyEyeStyle;
+
+  smileyEyeLeft.fullySelected = true;
+
+  let smileyEyeRight = smileyEyeLeft.clone();
+
+  smileyEyeRight.fullySelected = true;
+  smileyEyeRight.position.x += 70;
+
+  smileyEyeLeft.fullySelected = false;
+  smileyEyeRight.fullySelected = false;
+
+  let smileyMouth = new Path(new Point([160, 240]), new Point([240, 240]));
+
+  smileyMouth.style = smileyMouthStyle;
+
   // Draw the view now:
   view.draw();
 };
